6f4d00db23516bbc90a95a5484cd6bf7c99134e2
Muhammet ESER <muhammeteser29@gmail.com>
2019-03-14 19:15:25 +0300

    fixed product

--- components/Product.vue
+++ components/Product.vue
@@ -37,7 +37,7 @@ export default {
             });
           });
       } else {
-        this.$router.push("/login");
+         this.$router.push({ name: 'giris' });
       }
     },
     turkishtoEnglish(str) {
